333fd262a7a081b97e36e31bc0ab3f3e9c3b2545
lucascardenas7 <lucas13rojo@gmail.com>
2022-10-06 10:40:25 -0300

    Funcion suma

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ const peso=`67`;
 let arreglo=[nombre, apellido, edad, peso];
 console.log (arreglo)
 //#endregion
+
+//#region 
+
+var numero1="2";
+var numero2="2";
+var suma="0";
+var resultado=(numero)=>{
+    var suma = numero1 + numero2;
+    return suma;
+}
+console.log(suma)
+//#endregion
